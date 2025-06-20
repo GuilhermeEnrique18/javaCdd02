f75eb67e910d0b480fe956f7db6182cf3f5ff76e
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-20 15:48:35 -0300

    exercicio 03 utilizando metodo .equals para comparar se duas strings sao iguais ou nao

---
+++ src/Exercicio03.java
@@ -0,0 +1,14 @@
+public class Exercicio03 {
+    public static void main(String[] args) {
+
+    String t01 = "Será que são iguais?";
+    String t02 = "Será que são iguais?";
+
+        if(t01.equals(t02)){
+            System.out.println("Os textos são iguais.");
+
+        }else{
+            System.out.println("Os textos são diferentes.");
+        }
+    }
+}
