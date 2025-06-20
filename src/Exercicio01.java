1a923714999ea913c6594f5f767510ec87b5883e
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-20 14:45:09 -0300

    adicao exercicio 01 usando metodo trim para remover espacos em branco no inicio e no fim de uma string

---
+++ src/Exercicio01.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+        String str = " texto para retirar espaçoes no início e fim ";
+        System.out.println(str.trim());
+        
+    }
+}
